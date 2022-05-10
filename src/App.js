5c4b84077f7ddc682ba43087f1c853bc841cdc31
Sergey Lebedev <john_ross@mail.ru>
2022-05-11 01:20:02 +0300

    hw3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
 import './App.css';
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {Button, TextField} from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
+import BasicList from "./MessageList";
 
 function App() {
-  const[messageList, setMessageList] = useState([]);
-  const[author, setAuthor] = useState("");
-  const[message, setMessage] = useState("");
-  let idCounter = messageList.length;
+    const [messageList, setMessageList] = useState([]);
+    const [author, setAuthor] = useState("");
+    const [message, setMessage] = useState("");
+    const [chatArray, setChatArray] = useState([]); //массив чатов
 
-  useEffect(() => {
-      if(messageList.length > 0)
-      setTimeout(() => alert("Message send"), 1500)
-  }, [messageList]);
+    let idCounter = messageList.length;
+    const textfieldRef = useRef(null);
 
-  return (
-    <div className="App">
-        <>
-            <input value={author} onChange={(e) => setAuthor(e.target.value)}/>
-            <br/>
-            <input value={message} onChange={(e) => setMessage(e.target.value)}/>
-            <br/>
-            <button onClick={() => {
-                setMessageList([...messageList, {
-                    author: author,
-                    text: message,
-                    id: idCounter
-                }])
-            }}>Send</button>
+    useEffect(() => {
+        textfieldRef.current?.focus();
+        if (messageList.length > 0)
+            setTimeout(() => alert("Message send"), 1500)
+    }, [messageList]);
 
-        {messageList.map((item) =>
-            <p key={item.id}>Author: {item.author}<br/>Say: {item.text}</p>
-        )}
+    return (
+        <div className="App">
+                <BasicList/>
+                <TextField
+                    id="outlined-helperText"
+                    label="Author"
+                    defaultValue="Example: Ivan"
+                    helperText="Enter your name"
+                    size="small"
+                    onChange={(e) => setAuthor(e.target.value)}
+                    inputRef={textfieldRef}/>
+                <TextField
+                    id="outlined-search"
+                    label="Enter your message"
+                    type="search"
+                    size="small"
+                    onChange={(e) => setMessage(e.target.value)}/>
+                <Button
+                    variant="contained"
+                    endIcon={<SendIcon/>}
+                    size="normal"
+                    onClick={() => {
+                        setMessageList([...messageList, {
+                            author: author,
+                            text: message,
+                            id: idCounter
+                        }]);
+                        setChatArray([...chatArray, {
+                            id: Math.random().toString(36).slice(2, 7), //не сам, спасибо stackoverflow
+                            name: author
+                        }])
+                    }}>Send</Button>
 
-        </>
-    </div>
-  );
+                {messageList.map((item) =>
+                    <p key={item.id}>Author: {item.author}<br/>Say: {item.text}</p>
+                )}
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 // const myName = "Sergey";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
